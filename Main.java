6a45f6782d832ba421118ac615d5bdf6c416ab52
MJ-BUC <m.bucaro@outlook.com>
2022-03-22 15:22:18 -0500

    file reading added

--- Main.java
+++ Main.java
@@ -6,7 +6,24 @@ import java.io.FileNotFoundException;
 class Maze {
 
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        String[] arr = new String[20];
+        int index = 0;
+
+        File mazeFile = new File("C:\\Users\\lunke\\OneDrive\\Documents\\Github\\10EMaze\\maze.txt");
+
+        try {
+            Scanner maze = new Scanner(mazeFile);
+            while(maze.hasNext()) {
+                String item = maze.next();
+                arr[index] = item;
+                index++;
+            }
+            maze.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("file not found!");
+        }
+
         Stack mystack = new Stack<>();
         mystack.push(0);
         mystack.push(1);
@@ -17,6 +34,17 @@ class Maze {
 
         for (int i = 0; i < 6; i ++) {
             System.out.println(mystack.pop());
+        }
+
+        //debug
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        //debug
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.print(arr[i]);
+            System.out.print("\n");
         }
     }
 }
